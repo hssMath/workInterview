3129e5aa8b299d6f30df1ad994ecf7c362e9c0bb
hss <1395643963@qq.com>
2021-09-24 06:44:43 +0000

    git-svn-id: https://localhost/svn/workInterview/trunk@2 5072c65a-3829-fe46-9a92-c4b25b112d62

---
+++ src/A3_Weiyunsuan.java
@@ -0,0 +1,25 @@
+import jdk.nashorn.internal.objects.annotations.Where;
+
+import java.util.Scanner;
+
+public class A3_Weiyunsuan {
+    /**
+     * 描述:输入一个int型的正整数，计算出该int型数据在内存中存储时1的个数。
+     * 输入描述：输入一个整数（int类型）
+     * 输出描述：这个数转换成2进制后，输出1的个数
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()){
+            int num = sc.nextInt();
+            String str = Integer.toBinaryString(num);
+            num = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '1') {
+                    num++;
+                }
+            }
+            System.out.println(num);
+        }
+    }
+}
---
+++ src/TestList.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/**
+ * 常见的数据结构：Array,line List,Stack，List，Linked List，Queue，Tree,Graph,Hash,Heap.
+ * List的底层原理：
+ *      给了一个长度默认为10的object数组来进行元素的添加、插入、删除操作。
+ */
+public class TestList {
+    public static void main(String[] args) {
+        //public class ArrayList<E>     extends AbstractList<E>            implements List<E>, RandomAccess, Cloneable, java.io.Serializable
+        List<String> list = new ArrayList<>();
+        list.add("123");
+        list.add("456");
+        System.out.println(list);
+
+        for (int i = 0; i <list.size() ; i++) {
+            System.out.println(list.get(i).toString());
+        }
+
+        for (final String s:list){
+            System.out.println(s.toString());
+        }
+
+        for(Iterator iter = list.iterator();iter.hasNext();){
+            System.out.println(iter.next());
+        }
+
+        //public class Vector<E>        extends AbstractList<E>            implements List<E>, RandomAccess, Cloneable, java.io.Serializable
+        List<String> vector = new Vector<>();
+
+        //public class LinkedList<E>    extends AbstractSequentialList<E>  implements List<E>, Deque<E>, Cloneable, java.io.Serializable
+        List<String> linkedList = new LinkedList<>();
+    }
+}
