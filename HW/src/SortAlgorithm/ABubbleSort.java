89fac2887d818f8d2018efcf13c7ee343646c3e6
hss <1395643963@qq.com>
2021-12-08 02:39:07 +0000

    冒泡排序从大到小，从小到大排序。
    git-svn-id: https://localhost/svn/workInterview/trunk@19 5072c65a-3829-fe46-9a92-c4b25b112d62

--- HW/src/SortAlgorithm/ABubbleSort.java
+++ HW/src/SortAlgorithm/ABubbleSort.java
@@ -4,15 +4,17 @@ import java.util.Arrays;
 
 public class ABubbleSort {
 	public static void main(String[] args) {
-		int []arr = new int[]{2,8,5,8,9,7};
+		int []arr = new int[]{7,6,5,8,9,4,3,2};
 		System.out.println("冒泡排序前："+Arrays.toString(arr));
-		arr = ABubbleSortTest(arr);
-		System.out.println("冒泡排序后："+Arrays.toString(arr));
+		arr = ABubbleSortSmallToBigTest(arr);
+		System.out.println("冒泡（小->大）排序后："+Arrays.toString(arr));
+		arr = ABubbleSortBigToSmallTest(arr);
+		System.out.println("冒泡（大->小）排序后："+Arrays.toString(arr));
 	}
 	
 	/**
 	 * 
-	 * @Description:冒泡排序
+	 * @Description:冒泡（小->大）排序
 	 * @param:@param arr
 	 * @param:@return   
 	 * @author:heshuanshuan
@@ -20,15 +22,39 @@ public class ABubbleSort {
 	 * @return:int[] 
 	 * @history 修订历史（历次修订内容、修订人、修订时间等）
 	 */
-	public static int[] ABubbleSortTest(int arr[]) {
-		for (int i = 0; i < arr.length-1; i++) {//比较的轮数
-			for (int j = 0; j < arr.length-1-i; j++) {//每一轮比较的次数
+	public static int[] ABubbleSortSmallToBigTest(int[] arr) {
+		for (int i = 0; i < arr.length-1; i++) {//比较的轮数：下标从0开始，最后一个原色的下标为n-1
+			for (int j = 0; j < arr.length-1-i; j++) {//每一轮比较的次数:
 				if (arr[j]>arr[j+1]) {//每一轮都从0下标元素与0+1元素比较
 					int k = arr[j];
 					arr[j]=arr[j+1];
-					arr[j+1]=k; 
+					arr[j+1]=k;
 				}
 			}
+//			System.out.println(Arrays.toString(arr));
+		}
+		return arr;
+	}
+	/**
+	 *
+	 * @Description:冒泡（大->小）排序
+	 * @param:@param arr
+	 * @param:@return
+	 * @author:heshuanshuan
+	 * @date:2020年1月28日
+	 * @return:int[]
+	 * @history 修订历史（历次修订内容、修订人、修订时间等）
+	 */
+	public static int[] ABubbleSortBigToSmallTest(int[] arr) {
+		for (int i = 0; i < arr.length-1; i++) {//比较的轮数：下标从0开始，最后一个原色的下标为n-1
+			for (int j = 0; j < arr.length-1-i; j++) {//每一轮比较的次数:
+				if (arr[j]<arr[j+1]) {//每一轮都从0下标元素与0+1元素比较
+					int k = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=k;
+				}
+			}
+//			System.out.println(Arrays.toString(arr));
 		}
 		return arr;
 	}
