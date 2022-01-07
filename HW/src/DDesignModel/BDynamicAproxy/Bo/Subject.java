7c45d651eb069e14166727be73f3260920ce8e7b
hss <1395643963@qq.com>
2022-01-07 02:54:56 +0000

    包重命名
    git-svn-id: https://localhost/svn/workInterview/trunk@44 5072c65a-3829-fe46-9a92-c4b25b112d62

---
+++ HW/src/DDesignModel/BDynamicAproxy/Bo/Subject.java
@@ -0,0 +1,12 @@
+package DDesignModel.BDynamicAproxy.Bo;
+
+/**
+ * Subject
+ * 抽象主题接口
+ *
+ * @author
+ * @create 2018-03-29 14:16
+ **/
+public interface Subject {
+    void doSomething();
+}
---
+++ HW/src/DDesignModel/BDynamicAproxy/Impl/Subjectlmpl.java
@@ -0,0 +1,17 @@
+package DDesignModel.BDynamicAproxy.Impl;
+
+import DDesignModel.BDynamicAproxy.Bo.Subject;
+
+/**
+ * Subjectlmpl
+ * 真实主题类
+ *
+ * @author
+ * @create 2018-03-29 14:21
+ **/
+public class Subjectlmpl implements Subject {
+    @Override
+    public void doSomething() {
+        System.out.println("Subjectlmpl do something");
+    }
+}
