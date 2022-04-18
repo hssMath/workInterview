72cea0e822008142c34ada1049e86a6733dceeb2
hss <1395643963@qq.com>
2022-04-18 13:51:03 +0000

    N个元素使用冒泡排序（元素的交换位置，一组数据的多次排序）： 1、第一轮：让第一个下标为0的元素与第二个下标为1的元素进行比较，接着让第1个元素与第2个元素进行比较，...，直到下标为n-1-1的倒数第二个元素与第n个下标为n-1的元素进行比较，小的在前，大的在后。此时，比较了n-1次，最大值在最右边，因为两两比较中左边的数的下标最大为n-1-1。 2、第二轮：让第二个下标为1的元素与下标为2的元素进行比较，第2个元素与第3个元素对比，...，直到下标为n-1-1第n个下标为n-1的元素进行比较，小的在前，大的在后。此时，比较了(n-1)-1次，次大值在右边倒数第2个位置。 3、第三轮：让第三个下标为2的元素与第四个下标为3的元素进行比较，第3个元素与第4个元素对比，...，，直到下标为n-1-1第n个下标为n-1的元素进行比较，小的在前，大的在后。此时，比较了((n-1)-1)-1次，次次大值在右边倒数第3个位置。 直到，第n-1个元素与第n个元素对比，小的在前，大的在后。完成全部对比。 从第一轮到最后一轮，比较次数为：n-1,n-2,...3,2,1。 对应的时间复杂度：n的2次方。
    git-svn-id: https://localhost/svn/workInterview/trunk@57 5072c65a-3829-fe46-9a92-c4b25b112d62

--- HW/src/main/java/BSortAlgorithm/ABubbleSort.java
+++ HW/src/main/java/BSortAlgorithm/ABubbleSort.java
@@ -6,15 +6,15 @@ public class ABubbleSort {
 	public static void main(String[] args) {
 		int []arr = new int[]{7,6,5,8,9,10,4,3,2};
 		System.out.println("冒泡排序前："+Arrays.toString(arr));//数组打印为字符串可以直接使用：Arrays.toString(arr)
-		arr = ABubbleSortSmallToBigTest(arr);
-		System.out.println("冒泡（小->大）排序后："+Arrays.toString(arr));
-		arr = ABubbleSortBigToSmallTest(arr);
-		System.out.println("冒泡（大->小）排序后："+Arrays.toString(arr));
+		arr = ABubbleSortDescTest(arr);
+		System.out.println("冒泡升序排序后："+Arrays.toString(arr));
+		arr = ABubbleSortAscTest(arr);
+		System.out.println("冒泡降序排序后："+Arrays.toString(arr));
 	}
 	
 	/**
 	 * 
-	 * @Description:冒泡（小->大）排序
+	 * @Description:冒泡升序排序
 	 * @param:@param arr
 	 * @param:@return   
 	 * @author:heshuanshuan
@@ -22,8 +22,8 @@ public class ABubbleSort {
 	 * @return:int[] 
 	 * @history 修订历史（历次修订内容、修订人、修订时间等）
 	 */
-	public static int[] ABubbleSortSmallToBigTest(int[] arr) {
-		for (int i = 0; i < arr.length-1; i++) {//比较的轮数：下标从0开始，最后一个原色的下标为n-1。
+	public static int[] ABubbleSortDescTest(int[] arr) {
+		for (int i = 0; i < arr.length-1; i++) {//比较的轮数：下标从0开始，最后一个元素的下标为n-1。
 			for (int j = 0; j < arr.length-1-i; j++) {//每一轮比较的次数:从下标0开始，截至n-1-i。每一个循环结束，最后一个位置最大or最小。故循环次数为arr.length-1-i。
 				if (arr[j]>arr[j+1]) {//每一轮都从0下标元素与0+1元素比较，交换位置。
 					int k = arr[j];
@@ -31,13 +31,13 @@ public class ABubbleSort {
 					arr[j+1]=k;
 				}
 			}
-//			System.out.println(Arrays.toString(arr));
+			System.out.println(Arrays.toString(arr));
 		}
 		return arr;
 	}
 	/**
 	 *
-	 * @Description:冒泡（大->小）排序
+	 * @Description:冒泡降序排序
 	 * @param:@param arr
 	 * @param:@return
 	 * @author:heshuanshuan
@@ -45,7 +45,7 @@ public class ABubbleSort {
 	 * @return:int[]
 	 * @history 修订历史（历次修订内容、修订人、修订时间等）
 	 */
-	public static int[] ABubbleSortBigToSmallTest(int[] arr) {
+	public static int[] ABubbleSortAscTest(int[] arr) {
 		for (int i = 0; i < arr.length-1; i++) {//比较的轮数：下标从0开始，最后一个原色的下标为n-1。
 			for (int j = 0; j < arr.length-1-i; j++) {//每一轮比较的次数:从下标0开始，截至n-1-i。每一个循环结束，最后一个位置最大or最小。故循环次数为arr.length-1-i。
 				if (arr[j]<arr[j+1]) {//每一轮都从0下标元素与0+1元素比较，交换位置。
@@ -54,7 +54,7 @@ public class ABubbleSort {
 					arr[j+1]=k;
 				}
 			}
-//			System.out.println(Arrays.toString(arr));
+			System.out.println(Arrays.toString(arr));
 		}
 		return arr;
 	}
