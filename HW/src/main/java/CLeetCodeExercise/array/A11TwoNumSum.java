0250cccc031162a36fbc612a05cc08a00b22624e
hss <1395643963@qq.com>
2023-03-29 15:21:11 +0000

    Java中强大的jdk给我们提供了许多现成能使用的排序算法： 1、对数组排序    Arrays.sort(待排序数组名称); 2、对List排序排序    1.使用list自带的sort方法    2.使用Collections.sort();方法
    git-svn-id: https://localhost/svn/workInterview/trunk@91 5072c65a-3829-fe46-9a92-c4b25b112d62

--- HW/src/main/java/CLeetCodeExercise/array/A11TwoNumSum.java
+++ HW/src/main/java/CLeetCodeExercise/array/A11TwoNumSum.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 /**
- * 两数之和：给一个数组和一个目标数 target，获取2数之和的数的下标。
+ * NC61 两数之和：给出一个整型数组 numbers 和一个目标值 target，请在数组中找出两个加起来等于目标值的数的下标，返回的下标按升序排列。
+ * 注意：（注：返回的数组下标从1开始算起，保证target一定可以由数组里面2个数字相加得到）
  */
 public class A11TwoNumSum {
     public static void main(String[] args) {
-        System.out.println(getByHashMap(new int[]{2, 10, 1, 7, 9, 4}, 6));
+        System.out.println(getByHashMap(new int[]{20,70,110,150}, 90));
     }
 
     /**
      * 思路：
      *    使用 sum-数组中第i个数，引入 hashmap，若差能在 hashmap 中找到，就返回2个数组的下标；
      *    如果找不到就put到 hashmap 中：时间复杂度 => O(n)，最差的情况，循环遍历到最后一个数，才找到。
-     *
      * @param arr
      * @param target
      * @return
@@ -26,17 +26,17 @@ public class A11TwoNumSum {
 
         //引入hashmap存放没有找到的值
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        for (int i = 0; i <= arr.length - 1; i++) {//假如若有10个数，其最大下标为9。
-            int value = target - arr[i];//获取目标数与第i个数的差
-            Integer anotherIndex = hashMap.get(value);//在 hashmap 中是否能找到 key 为差值的下标
-            if (null != anotherIndex){//找到了，返回当前的下标i和另外一个下标
-                result[0] = anotherIndex;
-                result[1] = i;
+        for (int i = 0; i < arr.length; i++) {// 假如若有10个数，其最大下标为9。
+            int value = target - arr[i];// 获取目标数与第i个数的差
+            if (hashMap.containsKey(value)){// 找到了，返回当前的下标i和另外一个下标
+                Integer anotherIndex = hashMap.get(value);// hashmap 中是否能找到 key 为差值的下标
+                result[0] = anotherIndex+1;
+                result[1] = i+1;
             }else {//如果没有找到，添加到map中
                 hashMap.put(arr[i],i);
             }
         }
+        Arrays.sort(result); // Arrays类之只是提供了默认的升序排列，要对基础类型的数组进行降序排序，使用for反转
         return Arrays.toString(result);
     }
 
